162988616a10ad94f137bb7af7b219d1ec6e9fe9
Alvaro Montenegro <amontenegro@kenos.com.br>
2008-01-16 20:13:34 +0000

    Created file to drop all old lbr tables

---
+++ db_scripts/DropAll_lbr.sql
@@ -0,0 +1,51 @@
+/*
+	Drop all Old LBR Tables
+*/
+
+DROP TABLE adempiere.lbr_bank;
+
+DROP TABLE adempiere.lbr_bankinfo;
+
+DROP TABLE adempiere.lbr_boleto;
+
+DROP TABLE adempiere.lbr_bpartnercategory;
+
+DROP TABLE adempiere.lbr_cfop;
+
+DROP TABLE adempiere.lbr_cfopline;
+
+DROP TABLE adempiere.lbr_cnab;
+
+DROP TABLE adempiere.lbr_docprint;
+
+DROP TABLE adempiere.lbr_docprintfield;
+
+DROP TABLE adempiere.lbr_fiscalgroup_bpartner;
+
+DROP TABLE adempiere.lbr_fiscalgroup_product;
+
+DROP TABLE adempiere.lbr_icmsmatrix;
+
+DROP TABLE adempiere.lbr_ncm;
+
+DROP TABLE adempiere.lbr_productcategory;
+
+DROP TABLE adempiere.lbr_tax;
+
+DROP TABLE adempiere.lbr_taxconfig_bpartner;
+
+DROP TABLE adempiere.lbr_taxconfig_bpgroup;
+
+DROP TABLE adempiere.lbr_taxconfig_product;
+
+DROP TABLE adempiere.lbr_taxconfig_productgroup;
+
+DROP TABLE adempiere.lbr_taxconfig_region;
+
+DROP TABLE adempiere.lbr_taxconfiguration;
+
+DROP TABLE adempiere.lbr_taxformula;
+
+DROP TABLE adempiere.lbr_taxline;
+
+
