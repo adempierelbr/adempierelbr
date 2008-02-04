428b69ebbfd886a1755776360c7636734af62897
Eduardo Montenegro <emontenegro@kenos.com.br>
2008-02-04 17:19:56 +0000

    Correção do tipo de item de "User Maintened" para "LBRA"

--- db_scripts/100b-trunk/014-Reference-LBR_Frequency.sql
+++ db_scripts/100b-trunk/014-Reference-LBR_Frequency.sql
@@ -1,6 +1,6 @@
 -- 04/02/2008 15h4min40s BRST
 -- Default comment for updating dictionary
-INSERT INTO AD_Reference (AD_Org_ID,AD_Reference_ID,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,ValidationType,AD_Client_ID) VALUES (0,1000028,TO_TIMESTAMP('2008-02-04 15:04:39','YYYY-MM-DD HH24:MI:SS'),100,'U','Y','LBR_Frequency',TO_TIMESTAMP('2008-02-04 15:04:39','YYYY-MM-DD HH24:MI:SS'),100,'L',0)
+INSERT INTO AD_Reference (AD_Org_ID,AD_Reference_ID,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,ValidationType,AD_Client_ID) VALUES (0,1000028,TO_TIMESTAMP('2008-02-04 15:04:39','YYYY-MM-DD HH24:MI:SS'),100,'LBRA','Y','LBR_Frequency',TO_TIMESTAMP('2008-02-04 15:04:39','YYYY-MM-DD HH24:MI:SS'),100,'L',0)
 ;
 
 -- 04/02/2008 15h4min40s BRST
@@ -10,7 +10,7 @@ INSERT INTO AD_Reference_Trl (AD_Language,AD_Reference_ID, Description,Help,Name
 
 -- 04/02/2008 15h5min30s BRST
 -- Default comment for updating dictionary
-INSERT INTO AD_Ref_List (AD_Org_ID,AD_Reference_ID,AD_Ref_List_ID,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,AD_Client_ID,Value) VALUES (0,1000028,1000043,TO_TIMESTAMP('2008-02-04 15:05:29','YYYY-MM-DD HH24:MI:SS'),100,'U','Y','Monthly',TO_TIMESTAMP('2008-02-04 15:05:29','YYYY-MM-DD HH24:MI:SS'),100,0,'M')
+INSERT INTO AD_Ref_List (AD_Org_ID,AD_Reference_ID,AD_Ref_List_ID,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,AD_Client_ID,Value) VALUES (0,1000028,1000043,TO_TIMESTAMP('2008-02-04 15:05:29','YYYY-MM-DD HH24:MI:SS'),100,'LBRA','Y','Monthly',TO_TIMESTAMP('2008-02-04 15:05:29','YYYY-MM-DD HH24:MI:SS'),100,0,'M')
 ;
 
 -- 04/02/2008 15h5min30s BRST
