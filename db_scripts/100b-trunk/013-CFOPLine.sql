8c46bb0f850112524725a5467d327be860f48648
Mario Grigioni <mgrigioni@kenos.com.br>
2008-02-04 15:58:14 +0000

    db_script/013-CFOPLine.sql

---
+++ db_scripts/100b-trunk/013-CFOPLine.sql
@@ -0,0 +1,220 @@
+-- 04/02/2008 10h54min17s BRST
+-- Default comment for updating dictionary
+UPDATE AD_Column SET IsMandatory='Y',Updated=TO_TIMESTAMP('2008-02-04 10:54:17','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=1000312
+;
+
+-- 04/02/2008 10h54min17s BRST
+-- Default comment for updating dictionary
+UPDATE AD_Field SET Name='BPartner Category', Description='Primary key table LBR_BPartnerCategory', Help='Primary key table LBR_BPartnerCategory' WHERE AD_Column_ID=1000312 AND IsCentrallyMaintained='Y'
+;
+
+-- 04/02/2008 10h54min22s BRST
+-- Default comment for updating dictionary
+UPDATE AD_Column SET IsMandatory='N',Updated=TO_TIMESTAMP('2008-02-04 10:54:22','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=1000312
+;
+
+-- 04/02/2008 10h54min22s BRST
+-- Default comment for updating dictionary
+UPDATE AD_Field SET Name='BPartner Category', Description='Primary key table LBR_BPartnerCategory', Help='Primary key table LBR_BPartnerCategory' WHERE AD_Column_ID=1000312 AND IsCentrallyMaintained='Y'
+;
+
+-- 04/02/2008 10h54min23s BRST
+-- Default comment for updating dictionary
+insert into t_alter_column values('lbr_cfopline','LBR_BPartnerCategory_ID','NUMERIC(10)',null,'NULL')
+;
+
+-- 04/02/2008 10h54min24s BRST
+-- Default comment for updating dictionary
+insert into t_alter_column values('lbr_cfopline','LBR_BPartnerCategory_ID',null,'NULL',null)
+;
+
+-- 04/02/2008 10h54min34s BRST
+-- Default comment for updating dictionary
+UPDATE AD_Column SET IsMandatory='N',Updated=TO_TIMESTAMP('2008-02-04 10:54:34','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=1000287
+;
+
+-- 04/02/2008 10h54min34s BRST
+-- Default comment for updating dictionary
+UPDATE AD_Field SET Name='Product Category', Description='Primary key table LBR_ProductCategory', Help='Primary key table LBR_ProductCategory' WHERE AD_Column_ID=1000287 AND IsCentrallyMaintained='Y'
+;
+
+-- 04/02/2008 10h54min36s BRST
+-- Default comment for updating dictionary
+insert into t_alter_column values('lbr_cfopline','LBR_ProductCategory_ID','NUMERIC(10)',null,'NULL')
+;
+
+-- 04/02/2008 10h54min36s BRST
+-- Default comment for updating dictionary
+insert into t_alter_column values('lbr_cfopline','LBR_ProductCategory_ID',null,'NULL',null)
+;
+
+-- 04/02/2008 10h56min5s BRST
+-- Default comment for updating dictionary
+UPDATE AD_Column SET IsMandatory='Y',Updated=TO_TIMESTAMP('2008-02-04 10:56:05','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=1000287
+;
+
+-- 04/02/2008 10h56min5s BRST
+-- Default comment for updating dictionary
+UPDATE AD_Field SET Name='Product Category', Description='Primary key table LBR_ProductCategory', Help='Primary key table LBR_ProductCategory' WHERE AD_Column_ID=1000287 AND IsCentrallyMaintained='Y'
+;
+
+-- 04/02/2008 10h56min14s BRST
+-- Default comment for updating dictionary
+insert into t_alter_column values('lbr_cfopline','LBR_ProductCategory_ID','NUMERIC(10)',null,'NULL')
+;
+
+-- 04/02/2008 10h56min14s BRST
+-- Default comment for updating dictionary
+insert into t_alter_column values('lbr_cfopline','LBR_ProductCategory_ID',null,'NOT NULL',null)
+;
+
+-- 04/02/2008 10h56min25s BRST
+-- Default comment for updating dictionary
+UPDATE AD_Column SET IsMandatory='Y',Updated=TO_TIMESTAMP('2008-02-04 10:56:25','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=1000312
+;
+
+-- 04/02/2008 10h56min25s BRST
+-- Default comment for updating dictionary
+UPDATE AD_Field SET Name='BPartner Category', Description='Primary key table LBR_BPartnerCategory', Help='Primary key table LBR_BPartnerCategory' WHERE AD_Column_ID=1000312 AND IsCentrallyMaintained='Y'
+;
+
+-- 04/02/2008 10h56min26s BRST
+-- Default comment for updating dictionary
+insert into t_alter_column values('lbr_cfopline','LBR_BPartnerCategory_ID','NUMERIC(10)',null,'NULL')
+;
+
+-- 04/02/2008 10h56min26s BRST
+-- Default comment for updating dictionary
+insert into t_alter_column values('lbr_cfopline','LBR_BPartnerCategory_ID',null,'NOT NULL',null)
+;
+
+-- 04/02/2008 10h57min28s BRST
+-- Default comment for updating dictionary
+UPDATE AD_Column SET IsMandatory='N',Updated=TO_TIMESTAMP('2008-02-04 10:57:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=1000312
+;
+
+-- 04/02/2008 10h57min28s BRST
+-- Default comment for updating dictionary
+UPDATE AD_Field SET Name='BPartner Category', Description='Primary key table LBR_BPartnerCategory', Help='Primary key table LBR_BPartnerCategory' WHERE AD_Column_ID=1000312 AND IsCentrallyMaintained='Y'
+;
+
+-- 04/02/2008 10h57min29s BRST
+-- Default comment for updating dictionary
+insert into t_alter_column values('lbr_cfopline','LBR_BPartnerCategory_ID','NUMERIC(10)',null,'NULL')
+;
+
+-- 04/02/2008 10h57min29s BRST
+-- Default comment for updating dictionary
+insert into t_alter_column values('lbr_cfopline','LBR_BPartnerCategory_ID',null,'NULL',null)
+;
+
+-- 04/02/2008 10h57min37s BRST
+-- Default comment for updating dictionary
+UPDATE AD_Column SET IsMandatory='N',Updated=TO_TIMESTAMP('2008-02-04 10:57:37','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=1000287
+;
+
+-- 04/02/2008 10h57min37s BRST
+-- Default comment for updating dictionary
+UPDATE AD_Field SET Name='Product Category', Description='Primary key table LBR_ProductCategory', Help='Primary key table LBR_ProductCategory' WHERE AD_Column_ID=1000287 AND IsCentrallyMaintained='Y'
+;
+
+-- 04/02/2008 10h57min38s BRST
+-- Default comment for updating dictionary
+insert into t_alter_column values('lbr_cfopline','LBR_ProductCategory_ID','NUMERIC(10)',null,'NULL')
+;
+
+-- 04/02/2008 10h57min38s BRST
+-- Default comment for updating dictionary
+insert into t_alter_column values('lbr_cfopline','LBR_ProductCategory_ID',null,'NULL',null)
+;
+
+-- 04/02/2008 11h0min47s BRST
+-- Default comment for updating dictionary
+UPDATE AD_SysConfig SET Value='N',Updated=TO_TIMESTAMP('2008-02-04 11:00:47','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_SysConfig_ID=50010
+;
+
+-- 04/02/2008 11h0min49s BRST
+-- Default comment for updating dictionary
+INSERT INTO AD_Reference (AD_Org_ID,AD_Reference_ID,Created,CreatedBy,Description,EntityType,IsActive,Name,Updated,UpdatedBy,ValidationType,AD_Client_ID) VALUES (0,1000027,TO_TIMESTAMP('2008-02-04 11:00:49','YYYY-MM-DD HH24:MI:SS'),100,'Substituição Tributária','U','Y','LBR_SubsTributaria',TO_TIMESTAMP('2008-02-04 11:00:49','YYYY-MM-DD HH24:MI:SS'),100,'L',0)
+;
+
+-- 04/02/2008 11h0min49s BRST
+-- Default comment for updating dictionary
+INSERT INTO AD_Reference_Trl (AD_Language,AD_Reference_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Reference_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Reference t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Reference_ID=1000027 AND EXISTS (SELECT * FROM AD_Reference_Trl tt WHERE tt.AD_Language!=l.AD_Language OR tt.AD_Reference_ID!=t.AD_Reference_ID)
+;
+
+-- 04/02/2008 11h1min19s BRST
+-- Default comment for updating dictionary
+UPDATE AD_Reference SET EntityType='LBRA',Updated=TO_TIMESTAMP('2008-02-04 11:01:18','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Reference_ID=1000027
+;
+
+-- 04/02/2008 11h2min25s BRST
+-- Default comment for updating dictionary
+INSERT INTO AD_Ref_List (AD_Org_ID,AD_Reference_ID,AD_Ref_List_ID,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,AD_Client_ID,Value) VALUES (0,1000027,1000040,TO_TIMESTAMP('2008-02-04 11:02:25','YYYY-MM-DD HH24:MI:SS'),100,'LBRA','Y','Sim',TO_TIMESTAMP('2008-02-04 11:02:25','YYYY-MM-DD HH24:MI:SS'),100,0,'S')
+;
+
+-- 04/02/2008 11h2min25s BRST
+-- Default comment for updating dictionary
+INSERT INTO AD_Ref_List_Trl (AD_Language,AD_Ref_List_ID, Description,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Ref_List_ID, t.Description,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Ref_List t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Ref_List_ID=1000040 AND EXISTS (SELECT * FROM AD_Ref_List_Trl tt WHERE tt.AD_Language!=l.AD_Language OR tt.AD_Ref_List_ID!=t.AD_Ref_List_ID)
+;
+
+-- 04/02/2008 11h2min36s BRST
+-- Default comment for updating dictionary
+INSERT INTO AD_Ref_List (AD_Org_ID,AD_Reference_ID,AD_Ref_List_ID,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,AD_Client_ID,Value) VALUES (0,1000027,1000041,TO_TIMESTAMP('2008-02-04 11:02:36','YYYY-MM-DD HH24:MI:SS'),100,'LBRA','Y','Não',TO_TIMESTAMP('2008-02-04 11:02:36','YYYY-MM-DD HH24:MI:SS'),100,0,'N')
+;
+
+-- 04/02/2008 11h2min36s BRST
+-- Default comment for updating dictionary
+INSERT INTO AD_Ref_List_Trl (AD_Language,AD_Ref_List_ID, Description,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Ref_List_ID, t.Description,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Ref_List t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Ref_List_ID=1000041 AND EXISTS (SELECT * FROM AD_Ref_List_Trl tt WHERE tt.AD_Language!=l.AD_Language OR tt.AD_Ref_List_ID!=t.AD_Ref_List_ID)
+;
+
+-- 04/02/2008 11h3min0s BRST
+-- Default comment for updating dictionary
+INSERT INTO AD_Ref_List (AD_Org_ID,AD_Reference_ID,AD_Ref_List_ID,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,AD_Client_ID,Value) VALUES (0,1000027,1000042,TO_TIMESTAMP('2008-02-04 11:03:00','YYYY-MM-DD HH24:MI:SS'),100,'LBRA','Y','Ambos',TO_TIMESTAMP('2008-02-04 11:03:00','YYYY-MM-DD HH24:MI:SS'),100,0,'A')
+;
+
+-- 04/02/2008 11h3min0s BRST
+-- Default comment for updating dictionary
+INSERT INTO AD_Ref_List_Trl (AD_Language,AD_Ref_List_ID, Description,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Ref_List_ID, t.Description,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Ref_List t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Ref_List_ID=1000042 AND EXISTS (SELECT * FROM AD_Ref_List_Trl tt WHERE tt.AD_Language!=l.AD_Language OR tt.AD_Ref_List_ID!=t.AD_Ref_List_ID)
+;
+
+-- 04/02/2008 11h3min18s BRST
+-- Default comment for updating dictionary
+UPDATE AD_Ref_List SET Name='Yes', Value='Y',Updated=TO_TIMESTAMP('2008-02-04 11:03:18','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Ref_List_ID=1000040
+;
+
+-- 04/02/2008 11h3min18s BRST
+-- Default comment for updating dictionary
+UPDATE AD_Ref_List_Trl SET IsTranslated='N' WHERE AD_Ref_List_ID=1000040
+;
+
+-- 04/02/2008 11h3min24s BRST
+-- Default comment for updating dictionary
+UPDATE AD_Ref_List SET Name='No',Updated=TO_TIMESTAMP('2008-02-04 11:03:24','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Ref_List_ID=1000041
+;
+
+-- 04/02/2008 11h3min24s BRST
+-- Default comment for updating dictionary
+UPDATE AD_Ref_List_Trl SET IsTranslated='N' WHERE AD_Ref_List_ID=1000041
+;
+
+-- 04/02/2008 11h3min30s BRST
+-- Default comment for updating dictionary
+UPDATE AD_Ref_List SET Name='Both', Value='B',Updated=TO_TIMESTAMP('2008-02-04 11:03:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Ref_List_ID=1000042
+;
+
+-- 04/02/2008 11h3min30s BRST
+-- Default comment for updating dictionary
+UPDATE AD_Ref_List_Trl SET IsTranslated='N' WHERE AD_Ref_List_ID=1000042
+;
+
+-- 04/02/2008 11h4min20s BRST
+-- Default comment for updating dictionary
+UPDATE AD_Column SET AD_Reference_Value_ID=1000027, AD_Reference_ID=17,Updated=TO_TIMESTAMP('2008-02-04 11:04:20','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=1000299
+;
+
+-- 04/02/2008 11h4min20s BRST
+-- Default comment for updating dictionary
+UPDATE AD_Field SET Name='Is Substituicao Tributaria', Description='Defines the Is Substituicao Tributaria Status', Help='Defines the Is Substituicao Tributaria Status' WHERE AD_Column_ID=1000299 AND IsCentrallyMaintained='Y'
+;
+
