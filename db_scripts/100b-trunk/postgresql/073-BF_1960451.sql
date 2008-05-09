fb38219df774e74393c003783a92f128ba59b384
Mario Grigioni <mgrigioni@kenos.com.br>
2008-05-09 12:44:18 +0000

    ajuste no script 073-BF_1960451.sql

--- db_scripts/100b-trunk/postgresql/073-BF_1960451.sql
+++ db_scripts/100b-trunk/postgresql/073-BF_1960451.sql
@@ -28,37 +28,37 @@ update lbr_cfopline set lbr_tax_id = lbr_tax_id_old::numeric; commit;
 alter table lbr_cfopline drop column lbr_tax_id_old; commit;
 
 alter table lbr_icmsmatrix rename lbr_tax_id to lbr_tax_id_old; commit;
-alter table lbr_icmsmatrix add column lbr_tax_id numeric not null; commit;
+alter table lbr_icmsmatrix add column lbr_tax_id numeric; commit;
 delete from lbr_icmsmatrix where lbr_tax_id_old IS NULL; commit;
 update lbr_icmsmatrix set lbr_tax_id = lbr_tax_id_old::numeric; commit;
 alter table lbr_icmsmatrix drop column lbr_tax_id_old; commit;
 
 alter table lbr_taxconfig_bpartner rename lbr_tax_id to lbr_tax_id_old; commit;
-alter table lbr_taxconfig_bpartner add column lbr_tax_id numeric not null default 0; commit;
+alter table lbr_taxconfig_bpartner add column lbr_tax_id numeric; commit;
 delete from lbr_taxconfig_bpartner where lbr_tax_id_old IS NULL; commit;
 update lbr_taxconfig_bpartner set lbr_tax_id = lbr_tax_id_old::numeric; commit;
 alter table lbr_taxconfig_bpartner drop column lbr_tax_id_old; commit;
 
 alter table lbr_taxconfig_bpgroup rename lbr_tax_id to lbr_tax_id_old; commit;
-alter table lbr_taxconfig_bpgroup add column lbr_tax_id numeric not null default 0; commit;
+alter table lbr_taxconfig_bpgroup add column lbr_tax_id numeric; commit;
 delete from lbr_taxconfig_bpgroup where lbr_tax_id_old IS NULL; commit;
 update lbr_taxconfig_bpgroup set lbr_tax_id = lbr_tax_id_old::numeric; commit;
 alter table lbr_taxconfig_bpgroup drop column lbr_tax_id_old; commit;
 
 alter table lbr_taxconfig_product rename lbr_tax_id to lbr_tax_id_old; commit;
-alter table lbr_taxconfig_product add column lbr_tax_id numeric not null default 0; commit;
+alter table lbr_taxconfig_product add column lbr_tax_id numeric; commit;
 delete from lbr_taxconfig_product where lbr_tax_id_old IS NULL; commit;
 update lbr_taxconfig_product set lbr_tax_id = lbr_tax_id_old::numeric; commit;
 alter table lbr_taxconfig_product drop column lbr_tax_id_old; commit;
 
 alter table lbr_taxconfig_productgroup rename lbr_tax_id to lbr_tax_id_old; commit;
-alter table lbr_taxconfig_productgroup add column lbr_tax_id numeric not null default 0; commit;
+alter table lbr_taxconfig_productgroup add column lbr_tax_id numeric commit;
 delete from lbr_taxconfig_productgroup where lbr_tax_id_old IS NULL; commit;
 update lbr_taxconfig_productgroup set lbr_tax_id = lbr_tax_id_old::numeric; commit;
 alter table lbr_taxconfig_productgroup drop column lbr_tax_id_old; commit;
 
 alter table lbr_taxconfig_region rename lbr_tax_id to lbr_tax_id_old; commit;
-alter table lbr_taxconfig_region add column lbr_tax_id numeric not null default 0; commit;
+alter table lbr_taxconfig_region add column lbr_tax_id numeric; commit;
 delete from lbr_taxconfig_region where lbr_tax_id_old IS NULL; commit;
 update lbr_taxconfig_region set lbr_tax_id = lbr_tax_id_old::numeric; commit;
 alter table lbr_taxconfig_region drop column lbr_tax_id_old; commit;
