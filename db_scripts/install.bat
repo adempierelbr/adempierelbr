e69509bea3b7e493d67dcb1f28c09f2b198da111
Mario Grigioni <mgrigioni@kenos.com.br>
2008-09-01 13:53:42 +0000

    Alteração nos scripts de instalação. Adicionado dois novos parâmetros: DATABASE e USER

--- db_scripts/install.bat
+++ db_scripts/install.bat
@@ -1,21 +1,22 @@
 @echo off
 
 IF (%1)==() GOTO END
-
+IF (%2)==() %2=adempiere
+IF (%3)==() %3=adempiere
 CLS
 
 ECHO AdempiereLBR
 ECHO .
-ECHO . Mario Grigioni (Kenos, www.kenos.com.br) - 08/01/2008
+ECHO . Mario Grigioni (Kenos, www.kenos.com.br) - 01/09/2008
 ECHO .
 
 SET /p PGPASSWORD=Password (ADEMPIERE): 
 
-FOR %%f IN (%1\*.sql) DO psql -d adempiere -U adempiere -f %%f
+FOR %%f IN (%1\*.sql) DO psql -d %2 -U %3 -f %%f
 PAUSE
 EXIT
 
 :END
-ECHO Usage %0 DIRECTORY
+ECHO Usage %0 DIRECTORY DATABASE USER
 PAUSE
 EXIT
--- db_scripts/install.sh
+++ db_scripts/install.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 if [ -z "$1" ]; then
-   echo "Usage: $0 DIRECTORY"
+   echo "Usage: $0 DIRECTORY DATABASE USER"
    exit 0
 fi
+if [ -z "$2" ]; then
+	$2 = adempiere
+fi
+if [ -z "$3" ]; then
+	$3 = adempiere
+fi
+
 echo "AdempiereLBR"
 echo "."
-echo ". Mario Grigioni (Kenos, www.kenos.com.br) - 08/01/2008"
+echo ". Mario Grigioni (Kenos, www.kenos.com.br) - 01/09/2008"
 echo "."
 echo "Password (ADEMPIERE): "
 read variavel
 export PGPASSWORD=$variavel
 for f in $(ls $1/*.sql); do
 echo ". Executando Script" $f
-psql -d adempiere -U adempiere -f $f
+psql -d $2 -U $3 -f $f
 done
