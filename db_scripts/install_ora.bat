a2bf083fb94d635aa10edc3a3b735b93f18e858e
Mario Grigioni <mgrigioni@kenos.com.br>
2009-02-24 12:34:34 +0000

    script install_ora.bat

---
+++ db_scripts/install_ora.bat
@@ -0,0 +1,23 @@
+@echo off
+
+IF (%1)==() GOTO END
+IF (%2)==() GOTO END
+IF (%3)==() GOTO END
+CLS
+
+ECHO AdempiereLBR
+ECHO .
+ECHO . Eduardo Montenegro (Kenos, www.kenos.com.br) - 23-Feb-2008
+ECHO .
+
+SET /p DBPASSWORD=Password (Usuário do Banco de Dados, ex.:ADEMPIERE): : 
+
+FOR %%f IN (%1\*.sql) DO sqlplus %3/%DBPASSWORD%@%2 @%%f
+PAUSE
+EXIT
+
+:END
+ECHO Usage: %0 DIRECTORY ORACLE_INSTANCE USER
+PAUSE
+EXIT
+
