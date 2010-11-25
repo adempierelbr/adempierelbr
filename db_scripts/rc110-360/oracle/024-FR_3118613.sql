05d7e1e6c9c613bfccac30dedcb3e224dca0d8e0
monhate <devnull@localhost>
2010-11-25 18:28:30 +0000

    Alteração do último script executado e exit para oracle.

--- db_scripts/rc110-360/oracle/024-FR_3118613.sql
+++ db_scripts/rc110-360/oracle/024-FR_3118613.sql
@@ -378,3 +378,6 @@ INSERT INTO AD_Process_Para (AD_Client_ID,AD_Element_ID,AD_Org_ID,AD_Process_ID,
 INSERT INTO AD_Process_Para_Trl (AD_Language,AD_Process_Para_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Process_Para_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Process_Para t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Process_Para_ID=1120001 AND NOT EXISTS (SELECT * FROM AD_Process_Para_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Process_Para_ID=t.AD_Process_Para_ID)
 ;
 
+UPDATE AD_SysConfig SET Value='RC110-360/02-FR_3118613.sql' WHERE AD_SysConfig_ID=1100006;
+
+exit
