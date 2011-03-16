e0e7a2f86ff7ad6b1fa5f2e0a7bc7e2fbd0868f2
Pablo Pigozzo <pablobp4@gmail.com>
2011-03-16 18:13:06 +0000

    Fazer o pagamento para o título utilizando a mesma organização da Fatura.

--- boleto/src/org/adempierelbr/util/ReturnCNABUtil.java
+++ boleto/src/org/adempierelbr/util/ReturnCNABUtil.java
@@ -123,7 +123,8 @@ public class ReturnCNABUtil
 						}
 
 						MPayment Payment = new MPayment(ctx,0,trx);
-
+						Payment.setAD_Org_ID(Invoice.getAD_Org_ID());
+						
 						Integer C_BankAccount_ID = boleto.getC_BankAccount_ID();
 						//
 						if (C_BankAccount_ID == null)
