35296d574103a54d65f3ef2bded32dcadb0f10a1
Claudemir Todo Bom <claudemir@todobom.com>
2012-03-03 11:17:35 -0300

    LBR-6 mais ajustes nos scripts

--- db_scripts/360-trunk/postgresql/050_1-BF_LBR-6_RemoverPriceEnteredBR.sql
+++ db_scripts/360-trunk/postgresql/050_1-BF_LBR-6_RemoverPriceEnteredBR.sql
@@ -28,3 +28,5 @@ DELETE FROM AD_Element_Trl WHERE AD_Element_ID=1103000
 DELETE FROM AD_Element WHERE AD_Element_ID=1103000
 ;
 
+UPDATE AD_SysConfig SET Value='360-trunk/050_1-BF_LBR-6_RemoverPriceEnteredBR.sql' WHERE AD_SysConfig_ID=1100006;
+
