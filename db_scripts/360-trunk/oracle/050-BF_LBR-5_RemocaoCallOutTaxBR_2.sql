ebb05ea9865550b1480778901cf04cbc9031f051
Pablo Pigozzo <pablobp4@gmail.com>
2012-03-03 11:45:47 -0300

    Corrigido Script 049 para ser tornar mais genérico e não quebrar os callouts de quem possuí callouts além dos "originais".
    Removido Script 050 pois o script 049 resolve o problema.

--- db_scripts/360-trunk/oracle/050-BF_LBR-5_RemocaoCallOutTaxBR_2.sql
+++
@@ -1,9 +0,0 @@
-
--- 03/03/2012 10h25min BRT
--- Remoção do CallOut taxBR que não é mais usado na linha do Pedido - pablobp
-UPDATE AD_Column SET CallOut = 'org.compiere.model.CalloutOrder.amt;' WHERE AD_Column_ID = 12875; 
-
-
-UPDATE AD_SysConfig SET Value='360-trunk/050-BF_LBR-5_RemocaoCallOutTaxBR_2.sql' WHERE AD_SysConfig_ID=1100006;
-
-EXIT
