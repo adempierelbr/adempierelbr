ea211e0e00af759e6d0a1255b8a4c17c290cf3b2
Claudemir Todo Bom <claudemir@todobom.com>
2012-03-03 09:59:04 -0300

    LBR-6 / remoção campo priceenteredbr

---
+++ db_scripts/360-trunk/postgresql/050-RemoverPriceEnteredBR.sql
@@ -0,0 +1,30 @@
+-- 03/03/2012 9h51min5s BRT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Field_Trl WHERE AD_Field_ID=1103000
+;
+
+-- 03/03/2012 9h51min5s BRT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Field WHERE AD_Field_ID=1103000
+;
+
+-- 03/03/2012 9h51min45s BRT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Column_Trl WHERE AD_Column_ID=1103000
+;
+
+-- 03/03/2012 9h51min45s BRT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Column WHERE AD_Column_ID=1103000
+;
+
+-- 03/03/2012 9h51min52s BRT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Element_Trl WHERE AD_Element_ID=1103000
+;
+
+-- 03/03/2012 9h51min52s BRT
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+DELETE FROM AD_Element WHERE AD_Element_ID=1103000
+;
+
