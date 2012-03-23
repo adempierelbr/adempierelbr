926b59073301e357b4bfb3032bfe889d72b08c4b
Ricardo Santana <rsantana@kenos.com.br>
2012-03-23 10:16:39 -0300

    Fix nos processos para considerar apenas os IDs de tabela.

--- db_scripts/processes_post_install/oracle/900-update_sequence.sql
+++ db_scripts/processes_post_install/oracle/900-update_sequence.sql
@@ -1,4 +1,6 @@
--- Atualiza AD_Sequence, intervalo 1000000 - 1999999 para AdempiereLBR
-UPDATE ADEMPIERE.AD_SEQUENCE SET CurrentNext = 2000000 WHERE CurrentNext < 2000000;
+-- 	Atualiza AD_Sequence, intervalo 1000000 - 1999999 para AdempiereLBR
+--	BF: 2796433 - Atualizar somente as sequencias que sao ID de tabelas.
+UPDATE AD_Sequence SET CurrentNext=2000000 WHERE CurrentNext<2000000 AND IsTableID='Y'
+;
 
-exit
+EXIT
