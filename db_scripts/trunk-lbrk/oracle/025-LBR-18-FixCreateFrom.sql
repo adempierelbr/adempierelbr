6501de573c2723da481b1b4f8a30623f5b3fc07c
Ricardo Santana <rsantana@kenos.com.br>
2012-03-23 16:16:07 -0300

    [ LBR-18 ] Script duplicado

--- db_scripts/trunk-lbrk/oracle/025-LBR-18-FixCreateFrom.sql
+++
@@ -1,5 +0,0 @@
--- 23/03/2012 15h55min56s BRT
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_ModelValidator (AD_Client_ID,AD_ModelValidator_ID,AD_Org_ID,Created,CreatedBy,Description,EntityType,IsActive,ModelValidationClass,Name,SeqNo,Updated,UpdatedBy) VALUES (0,1120003,0,TO_DATE('2012-03-23 15:55:55','YYYY-MM-DD HH24:MI:SS'),100,'Validator LBR Commons','LBRA','Y','org.adempierelbr.validator.VLBRCommons','Validator LBR Commons',3,TO_DATE('2012-03-23 15:55:55','YYYY-MM-DD HH24:MI:SS'),100)
-;
-
