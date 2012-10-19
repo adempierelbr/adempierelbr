ba202e046ae1b4db43d06e2145b706e92b61b0c9
Ricardo Santana <rsantana@kenos.com.br>
2012-10-19 15:04:31 -0300

    Adaptador para o RPS funcionar via Web

---
+++ zkwebui/WEB-INF/src/org/adempierelbr/webui/adapter/RPSAdapter.java
@@ -0,0 +1,25 @@
+package org.adempierelbr.webui.adapter;
+
+import org.zkoss.util.media.AMedia;
+import org.zkoss.zul.Filedownload;
+
+/**
+ * 		Usado para processar o RPS já que os métodos Web ficam
+ * 			inacessíveis pelo processo.
+ * 
+ * 	@author Ricardo Santana (Kenos, www.kenos.com.br)
+ *	@version $Id: RPSAdapter.java, v1.0 2012/10/18 6:54:22 PM, ralexsander Exp $
+ */
+public class RPSAdapter
+{
+	/**
+	 * 	Constructor
+	 * 	@param fileName
+	 * 	@param rps
+	 */
+	public RPSAdapter (String fileName, StringBuffer rps)
+	{
+		AMedia media = new AMedia (fileName, null, null, rps.toString());
+		Filedownload.save(media);
+	}	//	RPSAdapter
+}	//	RPSAdapter
