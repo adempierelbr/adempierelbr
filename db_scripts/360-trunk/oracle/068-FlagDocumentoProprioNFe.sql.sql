71ec3d8950ce4f7091de1cd912bbf7c3e5e27803
Rogério Feitosa <rfeitosa@kenos.com.br>
2013-01-04 12:07:10 -0200

    Ajuste na Numeração dos Scripts Novos

--- db_scripts/360-trunk/oracle/068-FlagDocumentoProprioNFe.sql.sql
+++ db_scripts/360-trunk/oracle/068-FlagDocumentoProprioNFe.sql.sql
@@ -3,5 +3,5 @@
 UPDATE AD_Field SET DisplayLogic='@lbr_HasFiscalDocument@=''Y'' & @DocBaseType@=''API'' | @DocBaseType@=''ARC'' | @DocBaseType@=''NFB''',Updated=TO_DATE('2013-01-03 16:38:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=1000460
 ;
 --	Atualiza o ultimo script
-UPDATE AD_SysConfig SET Value='trunk-lbrk/27-FlagDocumentoProprioNFe.sql' WHERE AD_SysConfig_ID=1100006
+UPDATE AD_SysConfig SET Value='360-trunk/068-FlagDocumentoProprioNFe.sql' WHERE AD_SysConfig_ID=1100006
 ;
