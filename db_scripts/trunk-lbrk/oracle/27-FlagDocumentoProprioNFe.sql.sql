f4af4f8b17c8bc94fad4a1944301ec7fdc927859
Rogério Feitosa <rfeitosa@kenos.com.br>
2013-01-04 10:58:24 -0200

    Script para exibir o campo Documento Próprio nos Tipos de Documento da NFe.

---
+++ db_scripts/trunk-lbrk/oracle/27-FlagDocumentoProprioNFe.sql.sql
@@ -0,0 +1,7 @@
+-- 03/01/2013 16h38min26s BRST
+-- Default comment for updating dictionary
+UPDATE AD_Field SET DisplayLogic='@lbr_HasFiscalDocument@=''Y'' & @DocBaseType@=''API'' | @DocBaseType@=''ARC'' | @DocBaseType@=''NFB''',Updated=TO_DATE('2013-01-03 16:38:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=1000460
+;
+--	Atualiza o ultimo script
+UPDATE AD_SysConfig SET Value='trunk-lbrk/27-FlagDocumentoProprioNFe.sql' WHERE AD_SysConfig_ID=1100006
+;
