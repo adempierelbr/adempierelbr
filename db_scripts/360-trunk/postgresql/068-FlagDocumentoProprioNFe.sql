01899b2204b7d4ea9d0d3b9af4d22342335edcb7
Rogério Feitosa <rfeitosa@kenos.com.br>
2013-01-11 18:01:40 -0200

    Copy Right GPL

--- db_scripts/360-trunk/postgresql/068-FlagDocumentoProprioNFe.sql
+++ db_scripts/360-trunk/postgresql/068-FlagDocumentoProprioNFe.sql
@@ -1,3 +1,15 @@
+/******************************************************************************
+ * Copyright (C) 2013 Kenos Assessoria e Consultoria de Sistemas Ltda         *
+ * This program is free software; you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ *****************************************************************************/
 -- 03/01/2013 16h38min26s BRST
 -- Default comment for updating dictionary
 UPDATE AD_Field SET DisplayLogic='@lbr_HasFiscalDocument@=''Y'' & @DocBaseType@=''API'' | @DocBaseType@=''ARC'' | @DocBaseType@=''NFB''',Updated=TO_TIMESTAMP('2013-01-03 16:38:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=1000460
