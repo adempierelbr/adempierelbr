6f5ab0eb26e566d47238d8968d371773bbd81fa3
Rogério Feitosa <rfeitosa@kenos.com.br>
2013-02-14 15:21:17 -0200

    Script que desabilita o campo NFe Lote na Janela Nota Fiscal.

---
+++ db_scripts/360-trunk/postgresql/72-DesabilitarCampoLoteJanelaNotaFiscal_postgresql.sql
@@ -0,0 +1,10 @@
+-- 14/02/2013 14h15min16s BRST
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2013-02-14 14:15:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=2000339
+;
+
+-- 14/02/2013 14h15min51s BRST
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Field SET IsReadOnly='Y',Updated=TO_TIMESTAMP('2013-02-14 14:15:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=1100048
+;
+
