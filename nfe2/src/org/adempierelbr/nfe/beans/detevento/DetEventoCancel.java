a5d0589a29a57d53d2c888804b8a41f1bf63e16b
Priscila Pinheiro <ppinheir@kenos.com.br>
2013-03-28 15:18:43 -0300

    Alteração do Bean para Cancelamento de NF-e

--- nfe2/src/org/adempierelbr/nfe/beans/detevento/DetEventoCancel.java
+++ nfe2/src/org/adempierelbr/nfe/beans/detevento/DetEventoCancel.java
@@ -11,7 +11,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,    *
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
  *****************************************************************************/
-package org.adempierelbr.cce.beans.evento.infevento.detevento;
+package org.adempierelbr.nfe.beans.detevento;
 
 import org.adempierelbr.annotation.XMLFieldProperties;
 import org.adempierelbr.cce.beans.Signature;
@@ -25,25 +25,23 @@ import com.thoughtworks.xstream.annotations.XStreamAsAttribute;
  * 	@author Ricardo Santana (Kenos, www.kenos.com.br)
  *	@version $Id: InfEvento.java, v1.0 2012/05/12 16:44:58 PM, ralexsander Exp $
  */
-public class DetEvento
+public class DetEventoCancel
 {
 	@XStreamAsAttribute
 	@XMLFieldProperties	(id = "HP18")
 	private String versao;
 	
 	@XMLFieldProperties	(minSize=5, maxSize=60, id = "HP19")
-	private final String descEvento = "Carta de Corre\u00e7\u00e3o";
+	private final String descEvento = "Cancelamento de NF-e";
 	
-	@XMLFieldProperties	(minSize=15, maxSize=1000, id = "HP20")
-	private String xCorrecao;
+	@XMLFieldProperties	(minSize=15, maxSize=15, id = "HP20")
+	private String nProt;
 	
-	@XMLFieldProperties	(id = "HP20a")
-	private final String xCondUso = "A Carta de Corre\u00E7\u00E3o \u00E9 disciplinada pelo \u00A7 1\u00BA-A do art. 7\u00BA do Conv\u00EAnio S/N, " +
-			"de 15 de dezembro de 1970 e pode ser utilizada para regulariza\u00E7\u00E3o de erro ocorrido na emiss\u00E3o de documento fiscal, " +
-			"desde que o erro n\u00E3o esteja relacionado com: I - as vari\u00E1veis que determinam o valor do imposto tais como: base de c\u00E1lculo, " +
-			"al\u00EDquota, diferen\u00E7a de pre\u00E7o, quantidade, valor da opera\u00E7\u00E3o ou da presta\u00E7\u00E3o; " +
-			"II - a corre\u00E7\u00E3o de dados cadastrais que implique mudan\u00E7a do remetente ou do destinat\u00E1rio; " +
-			"III - a data de emiss\u00E3o ou de sa\u00EDda.";
+	@XMLFieldProperties	(minSize=15, maxSize=255, id = "HP21")
+	private String xJust;
+	
+	@XStreamAlias ("Signature")
+	private Signature signature;
 
 	public String getVersao()
 	{
@@ -60,21 +58,14 @@ public class DetEvento
 		return descEvento;
 	}	//	getDescEvento
 
-	public String getXCorrecao()
+	public String getNProt()
 	{
-		return xCorrecao;
-	}	//	getXCorrecao
-
-	public void setXCorrecao(String xCorrecao)
+		return nProt;
+	}	//	getNProt
+	
+	public String getXJust()
 	{
-		if (xCorrecao != null)
-			xCorrecao = xCorrecao.replace("\n", " | ");
-		this.xCorrecao = xCorrecao;
-	}	//	setXCorrecao
-
-	public String getXCondUso()
-	{
-		return xCondUso;
+		return xJust;
 	}	//	getXCondUso
 	
 }	//	DetEvento
