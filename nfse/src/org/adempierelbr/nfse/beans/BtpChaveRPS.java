b2b580ac3074d0e0107fa6f039602dc0df665a8d
Ricardo Santana <rsantana@kenos.com.br>
2013-07-02 12:10:12 -0300

    Modificação da Série do RPS para Alfanumérico

--- nfse/src/org/adempierelbr/nfse/beans/BtpChaveRPS.java
+++ nfse/src/org/adempierelbr/nfse/beans/BtpChaveRPS.java
@@ -61,8 +61,6 @@ public class BtpChaveRPS
 	 */
 	public void setSerieRPS(String serieRPS)
 	{
-		serieRPS = TextUtil.toNumeric(serieRPS);
-		//
 		if (serieRPS == null || serieRPS.length() < 1 
 				|| serieRPS.length() > 5)
 			log.warning("tpSerieRPS deve ter entre 1 e 5 dígitos");
