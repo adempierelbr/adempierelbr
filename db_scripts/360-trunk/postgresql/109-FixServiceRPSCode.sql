25414ea4234a2e4a500606c0265882a3a8c4db39
Ricardo Santana <rsantana@kenos.com.br>
2015-04-10 15:03:47 -0300

    Adição de Modelo de NF RPS

--- db_scripts/360-trunk/postgresql/109-FixServiceRPSCode.sql
+++ db_scripts/360-trunk/postgresql/109-FixServiceRPSCode.sql
@@ -1,6 +1,8 @@
 --	FIX RPS Code
 DELETE FROM AD_Ref_List WHERE AD_Reference_ID=1120009 AND Value='S1'
 ;
+DELETE FROM AD_Ref_List WHERE AD_Reference_ID=1120009 AND Value IN ('RPS', 'RP')
+;
 INSERT INTO AD_Ref_List (AD_Ref_List_ID, AD_Client_ID, AD_Org_ID, IsActive, Created, CreatedBy, Updated, UpdatedBy, Value, Name, Description, AD_Reference_ID, ValidFrom, ValidTo, EntityType) VALUES(1121554, 0, 0, 'Y', '2015-03-22 15:50:31.0', 100, '2015-03-22 15:50:31.0', 100, 'S1', 'Nota Fiscal de Serviços Eletrônica (RPS)', NULL, 1120009, NULL, NULL, 'LBRA')
 ;
 UPDATE LBR_NotaFiscal SET LBR_NFModel='S1' WHERE LBR_NFModel IN ('RPS', 'RP')
