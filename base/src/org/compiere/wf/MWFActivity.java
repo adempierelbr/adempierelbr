00dd367312cacb897cf27a406074b0c12fc421b5
Rogério Feitosa <rfeitosa@kenos.com.br>
2015-05-25 16:15:58 -0300

    Corrigido o código adicionando a instancia do objeto PO antes do  Assunto do Email para que o assunto também possa conter os dados do Objeto.

--- base/src/org/compiere/wf/MWFActivity.java
+++ base/src/org/compiere/wf/MWFActivity.java
@@ -1018,12 +1018,12 @@ public class MWFActivity extends X_AD_WF_Activity implements Runnable
 			{
 				MClient client = MClient.get(getCtx(), getAD_Client_ID());
 				MMailText mailtext = new MMailText(getCtx(),getNode().getR_MailText_ID(),null);
+				
+				mailtext.setPO(m_po);
 
 				String subject = getNode().getDescription()
 				+ ": " + mailtext.getMailHeader();
 				
-				mailtext.setPO(m_po);
-
 				String message = mailtext.getMailText(true)
 				+ "\n-----\n" + getNodeHelp();
 				String to = getNode().getEMail();
