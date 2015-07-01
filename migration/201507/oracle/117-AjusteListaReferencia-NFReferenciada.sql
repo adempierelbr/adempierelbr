60f808c7fcc9d5063189c89cd939d5b96b399190
Ricardo Santana <rsantana@kenos.com.br>
2015-07-01 13:44:49 -0300

    Correção nos scripts para Oracle

--- migration/201507/oracle/117-AjusteListaReferencia-NFReferenciada.sql
+++ migration/201507/oracle/117-AjusteListaReferencia-NFReferenciada.sql
@@ -11,3 +11,5 @@ UPDATE AD_Ref_List_Trl SET AD_Ref_List_ID = 1121569 WHERE AD_Ref_List_ID = 20002
 COMMIT;
 
 SELECT register_migration_script('117-AjusteListaReferencia-AcessoXMLNFe-NFReferenciada.sql') FROM dual;
+
+EXIT
