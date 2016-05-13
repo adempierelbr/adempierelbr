3427603cb4520090297922ab2dc1e277d7de7a7a
Ricardo Santana <rsantana@kenos.com.br>
2016-05-13 16:01:46 -0300

    Correção do arquivo de geração de SPED

--- sped/src/org/adempierelbr/sped/efd/bean/RC100.java
+++ sped/src/org/adempierelbr/sped/efd/bean/RC100.java
@@ -721,12 +721,10 @@ public class RC100 extends RegSped {
 
 			
 			// RC170 - deixar só o NUM_ITEM
-			List<RC170> getrC170 = getrC170();
-			for (int i = 0; i < getrC170.size(); i++)
+			for (RC170 rc170 : this.rC170)
 			{
-				RC170 rc170 = getrC170.get(i);
 				// remover dos itens o registro velho
-				getrC170().remove(rc170);
+				rC170.remove(rc170);
 				
 				// criar um novo, só com o núm item, obrigatório de acordo com essa exceção
 				RC170 new_rc170 = new RC170();
