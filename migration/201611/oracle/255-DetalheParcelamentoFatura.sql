18847ac7369b8e8e5df7269ef526bac4e41e9b31
Rogério Feitosa <rfeitosa@kenos.com.br>
2017-02-16 11:47:36 -0200

    Ajuste no script do Relatório de Parcelamento de Fatura adicionando o Tipo de Documento.

--- migration/201611/oracle/255-DetalheParcelamentoFatura.sql
+++ migration/201611/oracle/255-DetalheParcelamentoFatura.sql
@@ -1208,6 +1208,16 @@ UPDATE AD_PrintFormatItem SET SortNo=10,IsOrderBy='Y' WHERE AD_PrintFormatItem_I
 UPDATE AD_PrintFormatItem SET SortNo=20,IsOrderBy='Y' WHERE AD_PrintFormatItem_ID=1120129
 ;
 
+-- 16/02/2017 11h44min29s BRST
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Table_ID,ColumnName,Created,CreatedBy,Description,EntityType,FieldLength,Help,IsActive,IsAlwaysUpdateable,IsEncrypted,IsIdentifier,IsKey,IsMandatory,IsParent,IsSelectionColumn,IsTranslated,IsUpdateable,Name,Updated,UpdatedBy,Version) VALUES (0,1129791,196,0,19,1120578,'C_DocType_ID',TO_DATE('2017-02-16 11:44:28','YYYY-MM-DD HH24:MI:SS'),100,'Document type or rules','LBRA',10,'The Document Type determines document sequence and processing rules','Y','N','N','N','N','N','N','N','N','N','Document Type',TO_DATE('2017-02-16 11:44:28','YYYY-MM-DD HH24:MI:SS'),100,0)
+;
+
+-- 16/02/2017 11h44min29s BRST
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Column t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Column_ID=1129791 AND NOT EXISTS (SELECT * FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
+;
+
 SELECT Register_Migration_Script ('255-DetalheParcelamentoFatura.sql') FROM DUAL
 ;
 
