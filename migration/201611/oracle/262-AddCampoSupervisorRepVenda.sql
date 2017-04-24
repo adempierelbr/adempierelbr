06a06d4b07a795e2c34936b491ed9ccff120477f
Rogério Feitosa <rfeitosa@kenos.com.br>
2017-04-24 18:06:42 -0300

    Ajuste no Tipo de Entidade para LBRA.

--- migration/201611/oracle/262-AddCampoSupervisorRepVenda.sql
+++ migration/201611/oracle/262-AddCampoSupervisorRepVenda.sql
@@ -1,6 +1,6 @@
 -- 24/04/2017 17h8min11s BRT
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Reference_Value_ID,AD_Table_ID,ColumnName,Created,CreatedBy,Description,EntityType,FieldLength,Help,IsActive,IsAllowLogging,IsAlwaysUpdateable,IsAutocomplete,IsEncrypted,IsIdentifier,IsKey,IsMandatory,IsParent,IsSelectionColumn,IsSyncDatabase,IsTranslated,IsUpdateable,Name,SeqNo,Updated,UpdatedBy,Version) VALUES (0,1129797,1522,0,30,190,291,'Supervisor_ID',TO_DATE('2017-04-24 17:08:10','YYYY-MM-DD HH24:MI:SS'),100,'Supervisor for this user/organization - used for escalation and approval','U',22,'The Supervisor indicates who will be used for forwarding and escalating issues for this user - or for approvals.','Y','Y','N','N','N','N','N','N','N','N','N','N','Y','Supervisor',0,TO_DATE('2017-04-24 17:08:10','YYYY-MM-DD HH24:MI:SS'),100,0)
+INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Reference_Value_ID,AD_Table_ID,ColumnName,Created,CreatedBy,Description,EntityType,FieldLength,Help,IsActive,IsAllowLogging,IsAlwaysUpdateable,IsAutocomplete,IsEncrypted,IsIdentifier,IsKey,IsMandatory,IsParent,IsSelectionColumn,IsSyncDatabase,IsTranslated,IsUpdateable,Name,SeqNo,Updated,UpdatedBy,Version) VALUES (0,1129797,1522,0,30,190,291,'Supervisor_ID',TO_DATE('2017-04-24 17:08:10','YYYY-MM-DD HH24:MI:SS'),100,'Supervisor for this user/organization - used for escalation and approval','LBRA',22,'The Supervisor indicates who will be used for forwarding and escalating issues for this user - or for approvals.','Y','Y','N','N','N','N','N','N','N','N','N','N','Y','Supervisor',0,TO_DATE('2017-04-24 17:08:10','YYYY-MM-DD HH24:MI:SS'),100,0)
 ;
 
 -- 24/04/2017 17h8min11s BRT
@@ -20,7 +20,7 @@ ALTER TABLE C_BPartner ADD Supervisor_ID NUMBER(10) DEFAULT NULL
 
 -- 24/04/2017 17h9min10s BRT
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,Description,DisplayLength,EntityType,Help,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,SeqNo,SortNo,Updated,UpdatedBy) VALUES (0,1129797,1127182,0,225,TO_DATE('2017-04-24 17:09:09','YYYY-MM-DD HH24:MI:SS'),100,'Supervisor for this user/organization - used for escalation and approval',0,'U','The Supervisor indicates who will be used for forwarding and escalating issues for this user - or for approvals.','Y','Y','Y','N','N','N','N','N','Supervisor',80,0,TO_DATE('2017-04-24 17:09:09','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,Description,DisplayLength,EntityType,Help,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,SeqNo,SortNo,Updated,UpdatedBy) VALUES (0,1129797,1127182,0,225,TO_DATE('2017-04-24 17:09:09','YYYY-MM-DD HH24:MI:SS'),100,'Supervisor for this user/organization - used for escalation and approval',0,'LBRA','The Supervisor indicates who will be used for forwarding and escalating issues for this user - or for approvals.','Y','Y','Y','N','N','N','N','N','Supervisor',80,0,TO_DATE('2017-04-24 17:09:09','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
 -- 24/04/2017 17h9min10s BRT
