7b02dac62ff83e5e116e657e48a93952d6345c2a
Ricardo Santana <rsantana@kenos.com.br>
2017-09-14 12:16:02 -0300

    Preparação para release

--- migration/201710/postgresql/999-SetVersionLBR.sql
+++ migration/201710/postgresql/999-SetVersionLBR.sql
@@ -1,4 +1,4 @@
-UPDATE AD_SysConfig SET Value='v201709-Development' WHERE AD_SysConfig_ID=1100005
+UPDATE AD_SysConfig SET Value='v201710-Development' WHERE AD_SysConfig_ID=1100005
 ;
 
 -- 31/08/2015 11h54min22s BRT
