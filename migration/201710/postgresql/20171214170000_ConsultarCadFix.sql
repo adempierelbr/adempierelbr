5bbe4420ca656f59c143a8cacfee3aafe8968e7f
Ricardo Santana <rsantana@kenos.com.br>
2017-12-14 16:09:11 -0200

    Correção do parâmetro no processo de consultar cadastro, valor padrão

--- migration/201710/postgresql/20171214170000_ConsultarCadFix.sql
+++ migration/201710/postgresql/20171214170000_ConsultarCadFix.sql
@@ -1,5 +1,5 @@
 -- 14/12/2017 15h50min4s BRST
-UPDATE AD_Process_Para SET AD_Val_Rule_ID=202,Updated=TO_TIMESTAMP('2017-12-14 15:50:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=1120206
+UPDATE AD_Process_Para SET DefaultValue='@#AD_Org_ID@',AD_Val_Rule_ID=202,Updated=TO_TIMESTAMP('2017-12-14 15:50:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=1120206
 ;
 
 SELECT Register_Migration_Script ('20171214170000_ConsultarCadFix.sql') FROM DUAL
