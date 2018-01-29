96bcad11bc36c617c74c2c1371391cf0241fbd5f
Ricardo Santana <rsantana@kenos.com.br>
2018-01-29 15:09:23 -0200

    WebServices para recepção de eventos para MG

---
+++ migration/201710/oracle/201801291500_WSEventosMG.sql
@@ -0,0 +1,11 @@
+SET SQLBLANKLINES ON
+SET DEFINE OFF
+
+INSERT INTO LBR_NFeWebService(lbr_nfewebservice_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, name, c_region_id, lbr_nfeenv, versionno, url, lbr_wstype, lbr_nfewebservice_uu)
+  VALUES (1121677, 0, 0, 'Y', '2018-01-29 14:23:46', 100, '2018-01-29 14:23:46', 100, 'RecepcaoEvento', NULL, '1', '3.10', 'https://nfe.fazenda.mg.gov.br/nfe2/services/RecepcaoEvento', '1', 'E3FE588E-E7CF-4041-AF2E-F74B85F7E3BA')
+;
+INSERT INTO LBR_NFeWebService(lbr_nfewebservice_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, name, c_region_id, lbr_nfeenv, versionno, url, lbr_wstype, lbr_nfewebservice_uu)
+  VALUES (1121678, 0, 0, 'Y', '2018-01-29 14:23:46', 100, '2018-01-29 14:23:46', 100, 'RecepcaoEvento', NULL, '2', '3.10', 'https://hnfe.fazenda.mg.gov.br/nfe2/services/RecepcaoEvento', '1', '80739EA5-E6C6-4174-8B72-5A1815ED54E5')
+;
+SELECT Register_Migration_Script ('201801291500_WSEventosMG.sql') FROM DUAL
+;
