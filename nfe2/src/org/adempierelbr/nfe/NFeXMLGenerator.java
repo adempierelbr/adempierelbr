ddea52db7b01d94798f3d833be77caeba52f7ca3
Ricardo Santana <rsantana@kenos.com.br>
2018-10-23 16:51:45 -0300

    Sincronização com idempiere - correção do preenchimento do FCP no XML

--- nfe2/src/org/adempierelbr/nfe/NFeXMLGenerator.java
+++ nfe2/src/org/adempierelbr/nfe/NFeXMLGenerator.java
@@ -1208,7 +1208,7 @@ public class NFeXMLGenerator
 					if (fcpTax != null)
 					{
 						icms00.setPFCP(normalize4(fcpTax.getlbr_TaxRate()));
-						icms00.setVFCP(normalize4(fcpTax.getlbr_TaxAmt()));
+						icms00.setVFCP(normalize(fcpTax.getlbr_TaxAmt()));
 					}
 				}
 				else if (CST_ICMS_10.equals (taxStatus))
