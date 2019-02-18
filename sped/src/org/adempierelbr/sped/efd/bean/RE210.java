07cdb762a9d55df485a6b94458563f76c347f26a
Ricardo Santana <rsantana@kenos.com.br>
2019-02-18 16:46:30 -0300

    Remover System.out.println

--- sped/src/org/adempierelbr/sped/efd/bean/RE210.java
+++ sped/src/org/adempierelbr/sped/efd/bean/RE210.java
@@ -221,10 +221,6 @@ public class RE210 extends RegSped {
 			// se o saldo das devoluções for maior, zerar o registro RE250 e subtrair o saldo das devoluções 
 			else if(getrE250().get(getrE250().indexOf(reg)).getVL_OR().compareTo(SLD_VL_DEVOL)  <= 0)
 			{
-				//
-				System.out.println("getrE250().indexOf(reg)).getVL_OR() >> " + getrE250().get(getrE250().indexOf(reg)).getVL_OR());
-				
-				//
 				SLD_VL_DEVOL = SLD_VL_DEVOL.subtract(getrE250().get(getrE250().indexOf(reg)).getVL_OR());
 				getrE250().get(getrE250().indexOf(reg)).setVL_OR(Env.ZERO);
 			}				
